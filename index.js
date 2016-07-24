138d0976dd39eac8ce47c5fa5760eb439e0c5a65
Jon Lauridsen <mail@jonlauridsen.com>
2016-07-24 21:31:46 +0100

    Basic fake implementation based on specifications

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+module.exports = function (array, arrays) {
+  return [1, 2, 3, 4]
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,10 @@
-describe("foo", function () {
-  it("should be true", function () {
+"use strict";
+var expect = require("chai").expect
+var flatten = require("../")
+
+describe("flatten", function () {
+  it("should flatten arrays", function () {
+    var res = flatten([1, [2, [3]], 4])
+    expect(res).to.eql([1, 2, 3, 4])
   })
 })
