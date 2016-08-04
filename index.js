b4ce4e739b38c5fd1485ff5b37a6965860041976
Jon Lauridsen <mail@jonlauridsen.com>
2016-08-04 23:31:10 +0100

    camelCasing

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 "use strict";
 
-/**
- * Flatten all the elements.
- * E.g. [1, [2, [3] ], 4] -> [1, 2, 3, 4]
- *
- * @param {Array} array
- */
-module.exports = function (array) {
-  return recursive_flatten(array)
-}
-
-var recursive_flatten = function (array) {
+var recursiveFlatten = function (array) {
   var res = []
   for (let e of array) {
     if (Array.isArray(e)) {
-      res.push(...recursive_flatten(e))
+      res.push(...recursiveFlatten(e))
       continue
     }
     res.push(e)
   }
   return res
 }
+
+/**
+ * Flatten all the elements.
+ * E.g. [1, [2, [3] ], 4] -> [1, 2, 3, 4]
+ *
+ * @param {Array} array
+ */
+module.exports = recursiveFlatten
